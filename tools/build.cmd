29a99396a836bfc33f0046b99b0b9de89e6adc4b
Dem Pilafian <dem@centerkey.com>
2015-08-17 00:31:50 -0700

    Tool updates for native installers

--- tools/build.cmd
+++ tools/build.cmd
@@ -1,25 +1,64 @@
 @echo off
-::::::::::::::::::::::::::::::::
-:: Snap Backup                ::
-:: Build on Microsoft Windows ::
-::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::
+::  Snap Backup                   ::
+::  Build on Microsoft Windows XP ::
+::::::::::::::::::::::::::::::::::::
+
+:: JDK
+:: ===
+:: Install the Java SE Devloper Kit:
+::    http://www.oracle.com/technetwork/java/javase/downloads ("Windows x86")
+::
+:: Ant
+:: ===
+:: Download and unzip Ant into the "\Apps\Ant" folder:
+::    Download --> http://ant.apache.org/bindownload.cgi (".zip archive")
+::    Example install folder --> \Apps\Ant\apache-ant-1.9.6\bin
+::
+:: ImageMagick
+:: ===========
+:: Download and install ImageMagick with the default settings:
+::    http://www.imagemagick.org/script/binary-releases.php ("Win32 dynamic at 16 bits-per-pixel")
+::
+:: WiX Toolset
+:: ===========
+:: Download and install the WiX toolset with the default settings:
+::   http://wixtoolset.org ("RECOMMENDED DOWNLOAD")
 
 :: Set JAVA_HOME
 for /d %%i in ("\Program Files\Java\jdk*") do set JAVA_HOME=%%i
 
-:: set ANT_HOME
+:: Set ANT_HOME
 for /d %%i in ("\Apps\Ant\apache-ant*") do set ANT_HOME=%%i
 
-:: Display Variables and launch Ant
+:: Display variables and launch Ant
 set JAVA_HOME
 set ANT_HOME
-call %ANT_HOME%\bin\ant build
+call "%JAVA_HOME%\bin\javac" -version
+call "%ANT_HOME%\bin\ant" -version
+call "%ANT_HOME%\bin\ant" build
+echo.
+
+:: Create native installer (javapackager)
+call "%JAVA_HOME%\bin\javapackager" -version
+cd ..\build
+copy ..\src\resources\graphics\application\snap-backup-icon.png .
+convert snap-backup-icon.png SnapBackup.ico
+mkdir package\windows
+move SnapBackup.ico package\windows
+call "%JAVA_HOME%\bin\javapackager" -deploy -native msi ^
+   -srcfiles snapbackup.jar -appclass org.snapbackup.Main ^
+   -name SnapBackup -vendor "Snap Backup" -outdir deploy -outfile SnapBackup -v
+set /p version=<version.txt
+copy deploy\bundles\SnapBackup-1.0.msi snap-backup-installer-v%version%.msi
+echo.
 
 :: Copy to VirtualBox folder
 if exist \\VBOXSVR\VirtualBox_Share (
+   copy *.msi          \\VBOXSVR\VirtualBox_Share
+   copy ..\tools\*.cmd \\VBOXSVR\VirtualBox_Share
+   dir                 \\VBOXSVR\VirtualBox_Share
    echo.
-   copy ..\..\build\SnapBackupInstaller.exe \\VBOXSVR\VirtualBox_Share
-   dir \\VBOXSVR\VirtualBox_Share\SnapBackupInstaller.exe
    )
-echo.
+
 pause
--- tools/build.sh.command
+++ tools/build.sh.command
@@ -1,18 +1,19 @@
 #!/bin/sh
-###################
-##  Snap Backup  ##
-###################
+#########################
+##  Snap Backup        ##
+##  Build on Mac OS X  ##
+#########################
 
 # JDK
 # ===
 # Install the Java SE Devloper Kit for Mac OS X x64:
-#    http://www.oracle.com/technetwork/java/javase/downloads/
+#    http://www.oracle.com/technetwork/java/javase/downloads
 #
 # Ant
 # ===
-# Install Ant or download the binary distribution (.zip) and unzip into ~/apps/ant
-#    Download:  http://ant.apache.org/bindownload.cgi
-#    Example install folder:  ~/apps/ant/apache-ant-1.9.6/bin
+# Install Ant or download and unzip into the "~/apps/ant" folder:
+#    Download --> http://ant.apache.org/bindownload.cgi (".zip archive")
+#    Example install folder --> ~/apps/ant/apache-ant-1.9.6/bin
 
 addAnt() {
    antHome=~/apps/ant/$(ls ~/apps/ant | grep apache-ant | tail -1)
@@ -21,9 +22,9 @@ addAnt() {
    }
 
 buildMacInstaller() {
-   echo "=== Build native installer (javapackager) ==="
+   echo
+   $JAVA_HOME/bin/javapackager -version
    cd ../build
-   pwd
    cp ../src/resources/graphics/application/snap-backup-icon.png .
    mkdir SnapBackup.iconset
    sips -z 128 128 snap-backup-icon.png --out SnapBackup.iconset/icon_128x128.png
@@ -31,9 +32,11 @@ buildMacInstaller() {
    mkdir -p package/macosx
    mv SnapBackup.icns package/macosx
    $JAVA_HOME/bin/javapackager -deploy -native dmg \
-      -srcfiles snapbackup.jar -appclass org.snapbackup.Main -name SnapBackup \
-      -outdir deploy -outfile SnapBackup -v
-   cp deploy/bundles/SnapBackup-1.0.dmg SnapBackupInstaller-v$(cat version.txt).dmg
+      -srcfiles snapbackup.jar -appclass org.snapbackup.Main \
+      -name SnapBackup -vendor "Snap Backup" -outdir deploy -outfile SnapBackup -v
+   cp deploy/bundles/SnapBackup-1.0.dmg snap-backup-installer-v$(cat version.txt).dmg
+   pwd
+   ls -l *.dmg
    }
 
 echo
@@ -45,5 +48,4 @@ javac -version
 which ant || addAnt
 ant -version
 ant build
-echo
 buildMacInstaller
