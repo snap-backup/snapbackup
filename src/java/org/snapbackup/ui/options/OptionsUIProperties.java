b2f3d7c2d73e97c25cc92b18bc118beb6993a522
Dem Pilafian <dem@centerkey.com>
2016-10-02 12:16:32 -0700

    License update

--- src/java/org/snapbackup/ui/options/OptionsUIProperties.java
+++ src/java/org/snapbackup/ui/options/OptionsUIProperties.java
@@ -2,25 +2,10 @@
 // Snap Backup                                                                //
 // OptionsUIProperties.java                                                   //
 //                                                                            //
-// GNU General Public License:                                                //
-// This program is free software; you can redistribute it and/or modify it    //
-// under the terms of the GNU General Public License as published by the      //
-// Free Software Foundation; either version 2 of the License, or (at your     //
-// option) any later version.                                                 //
-//                                                                            //
-// This program is distributed in the hope that it will be useful, but        //
-// WITHOUT ANY WARRANTY; without even the implied warranty of                 //
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                       //
-//                                                                            //
-// See the GNU General Public License at http://www.gnu.org for more          //
-// details.                                                                   //
-//                                                                            //
-// Copyright (c) individual contributors to the Snap Backup project           //
-// Snap Backup is a registered trademark of Center Key                        //
-// http://snapbackup.org                                                      //
-//                                                                            //
 // Options UI Properties:                                                     //
 //    This object..                                                           //
+//                                                                            //
+// GPLv3 -- snapback.org/license -- Copyright (c) individual contributors     //
 ////////////////////////////////////////////////////////////////////////////////
 
 package org.snapbackup.ui.options;
--- src/resources/ReadMe.txt
+++ src/resources/ReadMe.txt
@@ -4,11 +4,11 @@ The one-click backup utility
 
 License
 -------
-Snap Backup is free for all to use.  To the maximum extent allowed by law,
-there are no warranties whatsoever included with this software.  Before
-relying on any backup files, be sure to check their validity and verify
-that the correct files were indeed backed up.  The source code for Snap
-Backup is available under the GNU General Public License.
+Snap Backup is open source and free for all to use.  To the maximum extent
+allowed by law, there are no warranties whatsoever included with this software.
+Before relying on any backup files, be sure to check their validity and verify
+that the correct files were indeed backed up.  The source code for Snap Backup
+is available under the GPLv3 license.
 
 Instructions
 ------------
@@ -16,11 +16,11 @@ On some systems with Java installed you can just double click the JAR file
 and Snap Backup will automatically run.  On other systems, you may have to
 enter the following command:
 
-  java -jar snapbackup.jar  
+  java -jar snapbackup.jar
 
 Consult your Java documentation for the exact steps to launch an executable
 JAR on your system.
 
 ============================
 http://snapbackup.org
-Copyright (c) individual contributors to the Snap Backup project
+Copyright (c) individual contributors
