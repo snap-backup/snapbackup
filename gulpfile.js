fd76ce5cc6d821fc0c64dbadc1e13f40ec2b5104
dpilafian <dem@centerkey.com>
2021-08-10 23:38:11 -0700

    Move tasks from gulp into npm scripts

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,12 @@
 
 // Imports
 import concat from        'gulp-concat';
-import del from           'del';
 import fileInclude from   'gulp-file-include';
 import gulp from          'gulp';
-import htmlHint from      'gulp-htmlhint';
 import mergeStream from   'merge-stream';
 import rename from        'gulp-rename';
 import size from          'gulp-size';
 import zip from           'gulp-zip';
-import { htmlValidator } from 'gulp-w3c-html-validator';
 import { readFileSync } from 'fs';
 
 // Setup
@@ -28,7 +25,6 @@ const download = {
    java: releaseUrl + installer.java + '?raw=true',
    past: releaseUrl + 'archive',
    };
-const htmlHintConfig = { 'attr-value-double-quotes': false };
 const context = {
    pkg:           pkg,
    pageTitle:     pkg.description,
@@ -45,40 +41,45 @@ const orgWebsite = {
 
 // Tasks
 const task = {
-   cleanWebsitesTarget() {
-      return del([websitesTargetFolder + '/**/*', '**/.DS_Store']);  //only delete folder contents so as to not kill webserver
-      },
    buildWebsites() {
       const processWeb = (topLevel) =>
          gulp.src(`websites/web/www.snapbackup.${topLevel}/**/*`)
             .pipe(fileInclude({ basepath: '@root', indent: true, context: context }))
+            .pipe(size({ showFiles: true }))
             .pipe(gulp.dest(`${websitesTargetFolder}/www.snapbackup.${topLevel}`));
       const processUserGuide = () =>
          gulp.src('src/resources/snap-backup-user-guide.html')
+            .pipe(size({ showFiles: true }))
             .pipe(gulp.dest(orgWebsite.root));
       const processProperties = () =>
          gulp.src('src/resources/properties/SnapBackup*.properties')
             .pipe(rename({ extname: '.properties.txt' }))
             .pipe(gulp.dest(orgWebsite.translate))
             .pipe(zip('SnapBackup.properties.zip'))
+            .pipe(size({ showFiles: true }))
             .pipe(gulp.dest(orgWebsite.translate));
       const processDefaultProperties = () =>
          gulp.src('src/resources/properties/SnapBackup.properties')
             .pipe(rename('SnapBackup_en.properties.txt'))
+            .pipe(size({ showFiles: true }))
             .pipe(gulp.dest(orgWebsite.translate));
       const processFlags = () =>
          gulp.src('src/resources/graphics/application/language-*.png')
+            .pipe(size({ showFiles: true }))
             .pipe(gulp.dest(orgWebsite.graphics));
       const processGraphics = () =>
          gulp.src('websites/graphics/**/*')
+            .pipe(size({ showFiles: true }))
             .pipe(gulp.dest(orgWebsite.graphics));
       const processCss = () =>
          gulp.src('websites/*.css')
             .pipe(concat('style.css'))
+            .pipe(size({ showFiles: true }))
             .pipe(gulp.dest(orgWebsite.root));
       const processJs = () =>
          gulp.src('websites/*.js')
             .pipe(concat('app.js'))
+            .pipe(size({ showFiles: true }))
             .pipe(gulp.dest(orgWebsite.root));
       return mergeStream(
          processWeb('com'),
@@ -93,17 +94,7 @@ const task = {
          processCss(),
          processJs());
       },
-   lintWebsites() {
-      return gulp.src(websitesTargetFolder + '/**/*.html')
-         .pipe(htmlHint(htmlHintConfig))
-         .pipe(htmlHint.reporter())
-         .pipe(htmlValidator.analyzer())
-         .pipe(htmlValidator.reporter())
-         .pipe(size({ showFiles: true }));
-      },
    };
 
 // Gulp
-gulp.task('clean', task.cleanWebsitesTarget);
-gulp.task('web',   task.buildWebsites);
-gulp.task('lint',  task.lintWebsites);
+gulp.task('web', task.buildWebsites);
